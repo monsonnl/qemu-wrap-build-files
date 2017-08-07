374d533026a47cdcf7411d067d404bafafa093ec
Nathaniel Monson <monsonnl@gmail.com>
2017-08-06 23:15:47 -0400

    update to generic naming that will be used for all architectures

--- cross-build.go
+++ cross-build.go
@@ -20,7 +20,7 @@ func crossBuildStart() {
 		log.Fatal(err)
 	}
 
-	err = os.Link("/usr/bin/resin-xbuild", "/bin/sh")
+	err = os.Link("/usr/bin/cross-build", "/bin/sh")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -40,11 +40,7 @@ func crossBuildEnd() {
 func runShell() error {
 	var cmd *exec.Cmd
 
-	if _, err := os.Stat("/usr/bin/qemu-arm-static"); err == nil {
-		cmd = exec.Command("/usr/bin/qemu-arm-static", append([]string{"-execve", "-0", os.Args[0], "/bin/sh"}, os.Args[1:]...)...)
-	} else {
-		cmd = exec.Command("/usr/bin/qemu-aarch64-static", append([]string{"-execve", "-0", os.Args[0], "/bin/sh"}, os.Args[1:]...)...)
-	}
+	cmd = exec.Command("/usr/bin/qemu-static", append([]string{"-execve", "-0", os.Args[0], "/bin/sh"}, os.Args[1:]...)...)
 
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
